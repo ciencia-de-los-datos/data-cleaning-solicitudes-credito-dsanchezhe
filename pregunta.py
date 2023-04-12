46ad9afcb7aa49392e9e030088b46859e6f58a5b
dsanchezhe <126032152+dsanchezhe@users.noreply.github.com>
2023-04-12 07:25:41 -0500

    Lab5

--- pregunta.py
+++ pregunta.py
@@ -6,53 +6,33 @@ Realice la limpieza del dataframe. Los tests evaluan si la limpieza fue realizad
 correctamente. Tenga en cuenta datos faltantes y duplicados.
 
 """
+from dateutil.parser import parse
 import pandas as pd
 
-def corregir_fecha(cadena):
-    formatos_fecha = ['%Y/%m/%d', '%d/%m/%Y', '%Y-%m-%d', '%d-%m-%Y', '%m/%d/%Y', '%Y/%d/%m']
-    for formato in formatos_fecha:
-        try:
-            return pd.to_datetime(cadena, format=formato)
-        except:
-            pass
-    return np.nan
 
 def clean_data():
 
-
-    df = pd.read_csv("solicitudes_credito.csv", sep=";",index_col='Unnamed: 0')
-    df=df.dropna()
+    df = pd.read_csv("solicitudes_credito.csv", sep=";",index_col=0)
+    df.reset_index(inplace=True,drop=True)
     
-    #Pasar todo a minusculas
-    df["sexo"]=df["sexo"].apply(lambda x:str(x).lower())
-
-    df["tipo_de_emprendimiento"]=df["tipo_de_emprendimiento"].apply(lambda y:str(y).lower())
+    df['fecha_de_beneficio'] = pd.to_datetime(df['fecha_de_beneficio'],dayfirst=True, format='mixed')
     
-    df["idea_negocio"]=df["idea_negocio"].apply(lambda y:str(y).lower())
-    df["idea_negocio"]=df["idea_negocio"].apply(lambda y:y.replace("_"," "))
-    df["idea_negocio"]=df["idea_negocio"].apply(lambda y:y.replace("-"," "))
-    df["idea_negocio"]=df["idea_negocio"].apply(lambda y: y[:-1] if (y[-1]==" ") else y)
+    df.dropna(axis='index',inplace=True)
+    df.drop_duplicates(inplace=True)
 
-    df["barrio"]=df["barrio"].apply(lambda y:str(y).lower())
+    df['sexo'] = df['sexo'].str.lower().astype(str).str.strip()
+    df['tipo_de_emprendimiento'] = df['tipo_de_emprendimiento'].str.lower().astype(str)
+    df['idea_negocio'] = df['idea_negocio'].str.lower().astype(str)
+    df['barrio'] = df['barrio'].str.lower().astype(str)
+    df['línea_credito'] = df['línea_credito'].str.lower().astype(str)
+     
+    df['idea_negocio'] = df['idea_negocio'].str.replace('_',' ').str.replace('-',' ').str.strip()
+    df['barrio'] = df['barrio'].str.replace('_','-').str.replace('-',' ')
+    df['línea_credito'] = df['línea_credito'].str.replace('_',' ').str.replace('-',' ').str.strip()
+
+    df['monto_del_credito'] = df['monto_del_credito'].str.replace(',','').str.replace('$','',regex=False).str.replace(' ','').str.strip().astype(float)   
     
-    df["barrio"]=df["barrio"].apply(lambda y:y.replace("_"," "))
-    df["barrio"]=df["barrio"].apply(lambda y:y.replace("-"," "))
+    df.drop_duplicates(inplace=True)
+    df.dropna(axis='index',inplace=True)
 
-    df["comuna_ciudadano"]=df["comuna_ciudadano"].apply(lambda y: int(y))
-
-    df["monto_del_credito"]=df["monto_del_credito"].apply(lambda y:y.replace(".00",""))
-    df["monto_del_credito"]=df["monto_del_credito"].apply(lambda y:y.replace(" ",""))
-    df["monto_del_credito"]=df["monto_del_credito"].apply(lambda y:y.replace(",",""))
-    df["monto_del_credito"]=df["monto_del_credito"].apply(lambda y:y.replace("$",""))
-    df["monto_del_credito"]=df["monto_del_credito"].apply(lambda y:int(y))
-
-    df["línea_credito"]=df["línea_credito"].apply(lambda y:str(y).lower())
-    df["línea_credito"]=df["línea_credito"].apply(lambda y:y.replace("empresarial-ed.-","empresarial ed."))
-    df["línea_credito"]=df["línea_credito"].apply(lambda y:y.replace("empresarial_ed._","empresarial ed."))
-    df["línea_credito"]=df["línea_credito"].apply(lambda y:y.replace("empresarial ed. ","empresarial ed."))
-
-    df["fecha_de_beneficio"] = df["fecha_de_beneficio"].apply(corregir_fecha)
-
-    df=df.drop_duplicates()
-   
     return df
